b1867b9fc1a6b32a65ddd7f7fec39446bdd0ae9f
maheshsp <maheshsp1809@gmail.com>
2023-06-07 19:38:58 +0530

    initial

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Starting server...");
+var score = 34;
+var score = 44; // redeclare it is allowed with var so var is normally not used.
+console.log("Score: " + score);
+let mahesh = 24;
